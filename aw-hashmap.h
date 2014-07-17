138994743ddc02ed1b5f38def9f377040c05482e
afterwise <afterwise@gmail.com>
2014-07-17 21:47:17 +0300

    Added cpp guards

--- aw-hashmap.h
+++ aw-hashmap.h
@@ -2,12 +2,20 @@
 #ifndef AW_HASHMAP_H
 #define AW_HASHMAP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
 
 void hashmap_init(unsigned *keys, size_t cnt);
 int hashmap_add(unsigned *keys, size_t cnt, unsigned key);
 int hashmap_remove(unsigned *keys, size_t cnt, unsigned key);
 int hashmap_find(unsigned *keys, size_t cnt, unsigned key);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* AW_HASHMAP_H */
 
